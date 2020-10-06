383fb32844a35c0c14462c96d1e81dcbdb51d93e
Wizard <rushimehta00@icloud.com>
2020-10-06 11:30:27 +0530

    2 project executed

--- 2.Dictionary/dictionary.py
+++ 2.Dictionary/dictionary.py
@@ -1,7 +1,7 @@
 import json
 from difflib import get_close_matches
 
-data = json.load(open("data.json"))
+data = json.load(open("2.Dictionary\data.json"))
 
 def translate(word):
     word = word.lower()
